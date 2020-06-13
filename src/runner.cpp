ef88cd887f2b6b7fefa0f17571faecc8bc8145c4
jmgelb <jmgelb@ncsu.edu>
2020-06-13 15:39:11 -0400

    runner: add support for running different projects

--- src/runner.cpp
+++ src/runner.cpp
@@ -8,6 +8,49 @@
 #include "test.h"
 #include "proj_1.h"
 
+void print_help_text()
+{
+    std::cout << "try one of these options:" << std::endl;
+    std::cout << "1) ./run.o --test" << std::endl;
+    std::cout << "2) ./run.o --proj1 Lambda K C L)" << std::endl;
+    std::cout << "3) ./run.o --proj2 Lambda Kcpu Kio C L M" << std::endl;
+    std::cout << "4) ./run.o --proj3 Lambda C L M" << std::endl;
+}
+
+void proj_1(const std::vector<std::string> & args)
+{
+    constexpr auto kLambdaIndex = 2;
+    constexpr auto kQueueSizeIndex = 3;
+    constexpr auto kCustomersToServeIndex = 4;
+    constexpr auto kLIndex = 5;
+
+    float lambda = 0;
+    std::size_t queue_size = 0;
+    std::size_t customers_to_serve = 0;
+    std::size_t L = 0;
+
+    std::stringstream(args[kLambdaIndex]) >> lambda;
+    std::stringstream(args[kQueueSizeIndex]) >> queue_size;
+    std::stringstream(args[kCustomersToServeIndex]) >> customers_to_serve;
+    std::stringstream(args[kLIndex]) >> L;
+
+    if (lambda == 0
+        || queue_size == 0
+        || customers_to_serve == 0
+        || L == 0) {
+        std::cout << "Invalid Args for proj1" << std::endl;
+        print_help_text();
+        return;
+    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+    run_project_1(lambda, queue_size, customers_to_serve, L);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::cout << customers_to_serve  << " customers took "
+              << duration.count() << " milliseconds!" << std::endl; 
+}
+
 int main(int argc, char ** argv) {
 
     // not needed but I don't want to work with char ** :)
@@ -17,46 +60,41 @@ int main(int argc, char ** argv) {
     }
 
     constexpr auto kTestArgsNumber = 2; // program name and --test
-    constexpr auto kRunArgsNumber = 5; // pname, Lambda, K, C, L
+    constexpr auto kProj1ArgsNumber = 6; // pname, mode, Lambda, K, C, L
+    constexpr auto kProj2ArgsNumber = 8; // pname, mode, lambda, kcpu, kio, C, L, M
+    constexpr auto kProj3ArgsNumber = 6; // pname, mode, lambda, C, L, M
     const auto received_params = args.size();
-    if (!(received_params == kTestArgsNumber || received_params == kRunArgsNumber)) {
-        std::cout << "Invalid Number of Args" << std::endl;
-        return 0;
-    }
 
-    constexpr auto kLambdaOrTestIndex = 1;
-    constexpr auto kQueueSizeIndex = 2;
-    constexpr auto kCustomersToServeIndex = 3;
-    constexpr auto kLIndex = 4;
+    constexpr auto kModeIndex = 1;
 
-    if (args[kLambdaOrTestIndex] == "--test") {
+    const auto & mode = args[kModeIndex];
+    if (mode == "--test") {
+        if (received_params != kTestArgsNumber) {
+            print_help_text();
+            return 0;
+        }
         testing::run_all_tests();
-        return 0;
+    } else if (mode == "--proj1") {
+        if (received_params != kProj1ArgsNumber) {
+            print_help_text();
+            return 0;
+        }
+        proj_1(args);
+    } else if (mode == "--proj2") {
+        if (received_params != kProj2ArgsNumber) {
+            print_help_text();
+            return 0;
+        }
+        std::cout << "TODO" << std::endl;
+    } else if (mode == "--proj3") {
+        if (received_params != kProj3ArgsNumber) {
+            print_help_text();
+            return 0;
+        }
+        std::cout << "TODO" << std::endl;
+    } else {
+        print_help_text();
     }
-
-    float lambda = 0;
-    std::size_t queue_size = 0;
-    std::size_t customers_to_serve = 0;
-    std::size_t L = 0;
-
-    std::stringstream(args[kLambdaOrTestIndex]) >> lambda;
-    std::stringstream(args[kQueueSizeIndex]) >> queue_size;
-    std::stringstream(args[kCustomersToServeIndex]) >> customers_to_serve;
-    std::stringstream(args[kLIndex]) >> L;
-
-    if (lambda == 0
-        || queue_size == 0
-        || customers_to_serve == 0
-        || L == 0) {
-        std::cout << "Invalid Args" << std::endl;
-        return 0;
-    }
-
-    auto start = std::chrono::high_resolution_clock::now();
-    run_project_1(lambda, queue_size, customers_to_serve, L);
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-    std::cout << customers_to_serve  << " customers took "
-              << duration.count() << " milliseconds!" << std::endl; 
-
+ 
+    return 0;
 }
